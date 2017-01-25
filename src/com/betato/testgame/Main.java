73f0bbef21d5eb9d86071b42747d2c462acaaa90
betato <battlestargatticus@gmail.com>
2017-01-24 21:05:33 -0500

    Add GameWindow test class

--- src/com/betato/testgame/Main.java
+++ src/com/betato/testgame/Main.java
@@ -2,6 +2,6 @@ package com.betato.testgame;
 
 public class Main {
 	public static void main(String[] args) {
-		
+		new TestGame();
 	}
 }
--- src/com/betato/testgame/TestGame.java
+++ src/com/betato/testgame/TestGame.java
@@ -1,5 +1,38 @@
 package com.betato.testgame;
 
-public class TestGame {
+import java.awt.Graphics;
+
+import com.betato.gamedisplay.GameLoop;
+import com.betato.gamedisplay.GameWindow;
+
+public class TestGame extends GameWindow{
+	private static final long serialVersionUID = 1L;
+
+	public TestGame() {
+		start(new GameLoop(60, 120));
+	}
+	
+	@Override
+	public void onInit() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onUpdate() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onRender(Graphics g) {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public void onExit() {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
