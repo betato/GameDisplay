da6997230b3d95711202c05dbe7b3ee7d288dcc6
betato <battlestargatticus@gmail.com>
2017-01-24 20:09:05 -0500

    Add game methods to GameWindow

--- src/com/betato/gamedisplay/GameWindow.java
+++ src/com/betato/gamedisplay/GameWindow.java
@@ -3,12 +3,35 @@ package com.betato.gamedisplay;
 import java.awt.Canvas;
 
 public abstract class GameWindow extends Canvas implements Game {
-
+	
 	private static final long serialVersionUID = 1L;
 	private GameLoop gameLoop;
+	
+	public int fps, ups = 0;
 	
 	public GameWindow(GameLoop gameLoop) {
 		this.gameLoop = gameLoop;
 		gameLoop.run(this);
+	}
+	
+	@Override
+	public void init() {
+		
+	}
+	
+	@Override
+	public void update() {
+		
+	}
+	
+	@Override
+	public void render() {
+		
+	}
+	
+	@Override
+	public void updateFps(int fps, int ups) {
+		this.fps = fps;
+		this.ups = ups;
 	}
 }
