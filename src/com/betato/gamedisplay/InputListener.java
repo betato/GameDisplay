04eceb46b665de357e8d7769e2b123ffbbb51481
betato <battlestargatticus@gmail.com>
2017-01-25 17:12:22 -0500

    Add mouse and key state tracking to InputListener

--- src/com/betato/gamedisplay/InputListener.java
+++ src/com/betato/gamedisplay/InputListener.java
@@ -1,5 +1,6 @@
 package com.betato.gamedisplay;
 
+import java.awt.MouseInfo;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.awt.event.MouseEvent;
@@ -9,17 +10,46 @@ import java.awt.event.MouseWheelEvent;
 import java.awt.event.MouseWheelListener;
 
 public class InputListener implements KeyListener, MouseListener, MouseMotionListener, MouseWheelListener{
-
+	
+	// Keyboard
+	public boolean[] keyState = new boolean[256];
+	public boolean[] keyDown = new boolean[256];
+	public boolean[] keyUp = new boolean[256];
+	
+	// Mouse
+	public int mouseX;
+	public int mouseY;
+	public int mouseWheel;
+	public boolean[] mouseState = new boolean[MouseInfo.getNumberOfButtons()];
+	public boolean[] mouseDown = new boolean[MouseInfo.getNumberOfButtons()];
+	public boolean[] mouseUp = new boolean[MouseInfo.getNumberOfButtons()];
+	
+	public void clear() {
+		// Clear keyboard
+		for (int i = 0; i < keyUp.length; i++)
+		{
+			keyDown[i] = false;
+			keyUp[i] = false;
+		}
+		
+		// Clear mouse
+		for (int i = 0; i < mouseUp.length; i++)
+		{
+			mouseDown[i] = false;
+			mouseUp[i] = false;
+		}
+	}
+	
 	@Override
 	public void keyPressed(KeyEvent e) {
-		// TODO Auto-generated method stub
-		
+		keyDown[e.getKeyCode()] = true;
+		keyState[e.getKeyCode()] = true;
 	}
 
 	@Override
 	public void keyReleased(KeyEvent e) {
-		// TODO Auto-generated method stub
-		
+		keyUp[e.getKeyCode()] = true;
+		keyState[e.getKeyCode()] = false;
 	}
 
 	@Override
@@ -30,8 +60,7 @@ public class InputListener implements KeyListener, MouseListener, MouseMotionLis
 
 	@Override
 	public void mouseWheelMoved(MouseWheelEvent e) {
-		// TODO Auto-generated method stub
-		
+		mouseWheel = e.getWheelRotation();
 	}
 
 	@Override
@@ -66,14 +95,14 @@ public class InputListener implements KeyListener, MouseListener, MouseMotionLis
 
 	@Override
 	public void mousePressed(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
+		mouseDown[e.getButton()] = true;
+		mouseState[e.getButton()] = true;
 	}
 
 	@Override
 	public void mouseReleased(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
+		mouseUp[e.getButton()] = true;
+		mouseState[e.getButton()] = false;
 	}
 	
 }
