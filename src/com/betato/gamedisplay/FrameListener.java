e238d40a7f40bc5a3e473c1fc5c4e740826cf53f
betato <battlestargatticus@gmail.com>
2017-02-03 18:03:44 -0500

    Add Javadoc comments for FrameListener

--- src/com/betato/gamedisplay/FrameListener.java
+++ src/com/betato/gamedisplay/FrameListener.java
@@ -7,53 +7,95 @@ import java.awt.event.WindowFocusListener;
 import java.awt.event.WindowListener;
 
 public class FrameListener implements WindowListener, WindowFocusListener, ComponentListener {
-	
-	GameWindow gameWindow;
-	
+
+	private GameWindow gameWindow;
+
+	/**
+	 * If the window has been resized since the last game update.
+	 */
 	public boolean windowResized = false;
+	/**
+	 * If the window has been moved since the last game update.
+	 */
 	public boolean windowMoved = false;
-	
+
+	/**
+	 * If the window has gained focus since the last game update.
+	 */
 	public boolean windowGainedFocus = false;
+	/**
+	 * If the window has lost focus since the last game update.
+	 */
 	public boolean windowLostFocus = false;
-	
+
+	/**
+	 * If the window has been minimized since the last game update.
+	 */
 	public boolean windowMinimized = false;
+	/**
+	 * If the window has been maximized since the last game update.
+	 */
 	public boolean windowMaximized = false;
-	
+
+	/**
+	 * If the window has been set as the active window since the last game
+	 * update.
+	 */
 	public boolean windowActivated = false;
+	/**
+	 * If the window has lost its status as active window since the last game
+	 * update.
+	 */
 	public boolean windowDeactivated = false;
-	
+
+	/**
+	 * If the window has been hidden since the last game update.
+	 */
 	public boolean windowHidden = false;
+	/**
+	 * If the window has been shown since the last game update.
+	 */
 	public boolean windowShown = false;
-	
+
+	/**
+	 * If the window has been made visible for the first time since the last
+	 * game update.
+	 */
 	public boolean windowOpened = false;
+	/**
+	 * If the window has closed since the last game update.
+	 */
 	public boolean windowClosed = false;
+	/**
+	 * If the window began closing since the last game update.
+	 */
 	public boolean windowClosing = false;
-	
+
 	public FrameListener(GameWindow gameWindow) {
 		this.gameWindow = gameWindow;
 	}
-	
+
 	public void clear() {
 		windowResized = false;
 		windowMoved = false;
-		
+
 		windowGainedFocus = false;
 		windowLostFocus = false;
-		
+
 		windowMinimized = false;
 		windowMaximized = false;
-		
+
 		windowActivated = false;
 		windowDeactivated = false;
-		
+
 		windowHidden = false;
 		windowShown = false;
-		
+
 		windowOpened = false;
 		windowClosed = false;
 		windowClosing = false;
 	}
-	
+
 	@Override
 	public void windowGainedFocus(WindowEvent arg0) {
 		windowGainedFocus = true;
